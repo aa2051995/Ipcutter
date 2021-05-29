5351a526e75658befedbc97d9e23a38f918d2f38
ahmedmansour <a.a2051995@gmail.com>
2021-05-29 19:34:36 +0200

    test in home

--- Shop/src/shop/Home.java
+++ Shop/src/shop/Home.java
@@ -1,13 +1,23 @@
 package shop;
 
-public class Home {
+public class Home extends Thread {
+  @Override
+public void run() {
 
+	super.run();
+}
 	public static void main(String[] args) {
+		
+		String hh = new String("hello");
+		if(hh.equals( "hello"))
+			System.out.println("hello");
 		String myip = args[0];
 		IPCutter cutter = new IPCutter();
 		int subs[] = cutter.getSubs(myip);
 		for(int sub :subs)
 			System.out.println(sub);
+//		Home home = new Home();
+//		home.run();
 	}
 
 }
